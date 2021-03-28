cb19cbbd66cd2a897268f892e2f0585c7bc3f948
Yash <ypt7895@gmail.com>
2021-03-29 01:27:39 +0530

    user routes

---
+++ routes/UserRouter.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/UserModel')
+
+async function hashPassword(password){
+    try{
+        let pass = await bcrypt.hash(password,10)
+        return pass
+    }catch(err){
+        return err
+    }
+}
+
+router.post('/signup',async (req,res)=>{
+    let pass = hashPassword(req.body.password)
+    const newUser = new User({
+        username: req.body.username,
+        password: pass
+    })
+    try{
+        const save = await newUser.save()
+        res.redirect('/login')
+    }catch(err){
+        res.redirect('/signup',{ message: 'Signup Failed' })
+    }
+})
+
+router.get('/login',(req,res)=>{
+    res.render('/login')
+})
+
+router.get('/signup',(req,res)=>{
+    res.render('/signup')
+})
+
+module.exports = router
