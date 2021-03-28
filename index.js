7da9bb96b2864d7682a022909741cdfe4c363f16
Yash <ypt7895@gmail.com>
2021-03-28 19:50:02 +0530

    packages

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error',(error)=>{
+    console.error(error)
+})
+db.once('open',()=>console.log("Connected to db"))
 
 app.set('views','./views')
 app.set('view engine','pug')
