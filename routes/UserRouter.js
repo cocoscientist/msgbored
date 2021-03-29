2e8f8cb20c8ac19472123b4716517813254fdb9c
Yash <ypt7895@gmail.com>
2021-03-29 18:26:14 +0530

    changes to router

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,15 +3,6 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/UserModel')
 
-/*async function hashPassword(password){
-    try{
-        let pass = await bcrypt.hash(password,10)
-        return pass
-    }catch(err){
-        return err
-    }
-}*/
-
 router.post('/signup',async (req,res)=>{
     let pass = await bcrypt.hash(req.body.password,10)
     const newUser = new User({
@@ -22,12 +13,8 @@ router.post('/signup',async (req,res)=>{
         const save = await newUser.save()
         res.redirect('/login')
     }catch(err){
-        res.redirect(400,'/signup',{ message: 'Signup Failed' })
+        res.redirect('/signup',{ message: 'Signup Failed' })
     }
-})
-
-router.get('/login',(req,res)=>{
-    res.render('login')
 })
 
 router.get('/signup',(req,res)=>{
